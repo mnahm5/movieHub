1067c5c7a29868118ac40661ea84079f7dc5d623
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-01-10 19:50:02 +1100

    Router  Movie Details

--- app/services/movie.service.ts
+++ app/services/movie.service.ts
@@ -17,13 +17,17 @@ export class MovieService{
   }
 
   getInTheaters(){
-    return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&primary_release_date.gte=2017-01-01&primary_release_date.lte=2017-01-09&api_key='+this.apikey)
+    return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&primary_release_date.gte=2016-09-26&primary_release_date.lte=2016-10-30&api_key='+this.apikey)
       .map(res => res.json());
   }
 
-  searchMovies(searchStr: string) {
-    console.log(searchStr);
-    return this._jsonp.get('https://api.themoviedb.org/3/search/movie?callback=JSONP_CALLBACK&query=' + searchStr + '&sort_by=popularity.desc&api_key='+this.apikey)
+  searchMovies(searchStr:string){
+    return this._jsonp.get('https://api.themoviedb.org/3/search/movie?callback=JSONP_CALLBACK&query='+searchStr+'&sort_by=popularity.desc&api_key='+this.apikey)
+      .map(res => res.json());
+  }
+
+  getMovie(id:string){
+    return this._jsonp.get('https://api.themoviedb.org/3/movie/'+id+'?callback=JSONP_CALLBACK&api_key='+this.apikey)
       .map(res => res.json());
   }
 }
